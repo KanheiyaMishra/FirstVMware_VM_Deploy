0b8f3ba36ccb95a20cc5b7e6da7b41a72de3025d
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-03-03 10:01:48 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,12 +1,3 @@
-
-
-provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
-}
-
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_datacenter
 }
