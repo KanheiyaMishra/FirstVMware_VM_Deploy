d49c680294b76e64f1fe36cf871f7577359847e5
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-03-03 10:02:50 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "Learn_Terraform_Kanheiya"
+    workspaces {
+      name = "FirstVMware_VM_Deploy"
+    }
+  }
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.6.1"
+    }
+  }
+}
+provider "vsphere" {}
