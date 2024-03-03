f5f7e5f766a2ba7144bfe558a08f05b1e50ab8a6
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-03-03 09:06:40 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.6.1"
+    }
+  }
+}
+
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
