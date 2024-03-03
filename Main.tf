083e339f67a56f1d73d8ae59b0cb0c9d13fada12
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-03-03 10:03:27 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,50 +1,24 @@
-data "vsphere_datacenter" "dc" {
-  name = var.vsphere_datacenter
-}
+resource "vsphere_virtual_machine" "this" {
+  name             = "intftest01lvd"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
-data "vsphere_network" "vm1_net" {
-  name          = var.VM_Network
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-
-data "vsphere_datastore" "ds" {
-  name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_compute_cluster" "cl" {
-  name          = var.vsphere_compute_cluster
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = var.vsphere_template
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-resource "vsphere_virtual_machine" "aci_vm1" {
-  count            = 1
-  name             = var.aci_vm1_name
-  resource_pool_id = data.vsphere_compute_cluster.cl.resource_pool_id
-  datastore_id     = data.vsphere_datastore.ds.id
-
-  num_cpus  = 4
+  num_cpus             = 4
   num_cores_per_socket = 2
-  memory    = 4096
-  guest_id  = data.vsphere_virtual_machine.template.guest_id
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
-  firmware = "bios"
+  memory               = 4096
+  guest_id             = data.vsphere_virtual_machine.template.guest_id
+  scsi_type            = data.vsphere_virtual_machine.template.scsi_type
+  firmware             = "efi"
   disk {
     label            = "disk0"
     size             = data.vsphere_virtual_machine.template.disks[0].size
     thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
   }
 
-  folder = var.folder
+  folder = "TBD_Kiran"
 
   network_interface {
-    network_id   = data.vsphere_network.vm1_net.id
+    network_id   = data.vsphere_network.network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
@@ -54,22 +28,19 @@ resource "vsphere_virtual_machine" "aci_vm1" {
 
     customize {
       linux_options {
-        host_name = var.aci_vm1_name
-        script_text = file (var.VM_Spec_Scripts)
-        domain    = var.domain_name
+        host_name   = "intftest01lvd"
+        script_text = file("./customization.sh")
+        domain      = "noam.fadv.net"
       }
 
       network_interface {
-        ipv4_address = var.aci_vm1_address
+        ipv4_address = "10.74.4.194"
         ipv4_netmask = "24"
       }
 
-      ipv4_gateway    = var.gateway
-      #dns_server_list = [var.dns_list]
-      #dns_suffix_list = [var.dns_search]
-      dns_server_list = flatten ([var.dns_list])
-      dns_suffix_list = flatten ([var.dns_search])
+      ipv4_gateway    = "10.74.4.1"
+      dns_server_list = ["10.74.95.32", "10.74.95.51"]
+      dns_suffix_list = ["noam.fadv.net", "apac.fadv.net", "scrn.fadv.net", "fadv.net"]
     }
   }
 }
-
